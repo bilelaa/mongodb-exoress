cddd94477c2a99ad7fb138a7882a1a0431b573a9
bilelaa <biilelbenhlel@icloud.com>
2024-04-18 13:26:11 +0200

    setup express server with nodemailer

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 
 const app = express();
-
+const dotenv= require ("dotenv");
+dotenv.config();
+app.use(express.json());
 app.use("/api",require("./lib/handler"));
-
-
-
-
 
 
 
 app.listen(8888, () => {
   console.log("server is up on 8888");
 });
+
